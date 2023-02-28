c988ed84be9ec32432441577451d72d7b8a7b958
devops-bh <2016bryh@gmail.com>
2023-02-28 15:05:47 +0000

    add [ngrok] deploy command to package.json

--- deploy.js
+++ deploy.js
@@ -1,6 +1,8 @@
 const ngrok = require("ngrok")
 require('dotenv').config()
 console.log("Please ensure your ngrok authtoken is placed in the .env file within the same directory as this file")
+// there's probably some trickery to have this script run npm run start as well as handle the ngrok stuff but this will do for now 
+console.log("Please ensure you have ran npm run start in a different terminal session")
 async function deploy() {
     await ngrok.authtoken(process.env.NGROK_AUTHTOKEN);
     const url = await ngrok.connect(3000); 

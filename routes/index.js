ea9526e6cc18d2d3886430bf142b561e6614817c
devops-bh <2016bryh@gmail.com>
2023-03-05 17:11:43 +0000

    added sign-in.ejs added puppeteer script to automate signing in

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// perhaps this belongs in routes/users.js 
+router.get('/sign-in', (req, res) => {
+  console.log("signin")
+  res.render('sign-in', { title: 'Sign in' })
+}) 
+router.post('/sign-in', (req, res,) => {
+  const { email, password } = req.body
+  if (email == "" || password == "") {
+    res.render('sign-in', {message: 'email/password missing'})
+    return
+  }
+  console.log("rendered")
+})
+
 module.exports = router;
---
+++ tests/confirm-user-can-sign-in.mjs
@@ -0,0 +1,13 @@
+// optional: make these proper unit tests, for now I'm just automating the sign in for quickness + convienence 
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false, slowMo: 500});
+  const page = await browser.newPage();
+
+  await page.goto('http://localhost:3000/sign-in', );
+  await page.type('#email', 'ross@ip3.com');
+  await page.type('#password', 'lecturer');
+  await page.click('#sign-in');
+  await page.waitForNavigation();
+})();

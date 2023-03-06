9e1f4175ff77633d806bb44597eea0a04f02afac
devops-bh <2016bryh@gmail.com>
2023-03-06 13:03:52 +0000

    implemented password hashing regarding signing in (for hardcoded user) and a coloured circle to visually indiciate the user's signed in status

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,49 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcrypt")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  req.session.user_id = null
-  res.render('index', { title: 'Express' });
-  next() 
+router.get('/', function(req, res) {
+  if (req.session.signed_in == true) {
+    res.render('index', { title: 'Litter Map', signed_in: true});
+    return; 
+  }
+  res.render('index', { title: 'Litter Map', signed_in: false });
 });
+
+
 
 // perhaps this belongs in routes/users.js 
 router.get('/sign-in', (req, res) => {
-  console.log("signin")
   res.render('sign-in', { title: 'Sign in' })
 }) 
 function isSignedIn(req, res, next) {
-  if (req.session.user_id == null) {
+  console.log("checking user is signed in")
+  if (req.session.signed_in == false) {
     res.redirect('/sign-in')
   } 
-  return next();
+  if (req.session.signed_in) {
+    res.redirect('')
+  }
+  next();
 }
+
 router.post('/sign-in', isSignedIn, (req, res) => {
   const { email, password } = req.body
+  console.log(email, password)
   if (email == "" || password == "") {
     res.redirect('/') // todo: implement warning using the connect-flash package 
+    return;
   }
-  // todo: get password & username from database 
-  const hashedPassword = "lecturer"
-  if (password == hashedPassword) { 
-    req.session.user_id = "ross" // todo: replace this with database id 
-    console.log("signed in")
-    res.render('/', {title: 'signed in as ross', id: req.session.user_id}) 
-  }
+  bcrypt.hash('lecturer', 1, (err, hashedPassword) => {
+    if (err) { throw (err); }
+    bcrypt.compare('lecturer', hashedPassword, (err, result) => {
+        if (err) { throw (err); }
+        req.session.signed_in = true 
+        console.log("user has just signed in: ", result) 
+        res.render('index', {title: 'signed in as ross', signed_in: req.session.signed_in}) 
+    });
+  });
 })
 
 module.exports = router;

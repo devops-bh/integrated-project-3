860c7c625707bf6a16708d394776b04aa689c157
devops-bh <2016bryh@gmail.com>
2023-03-15 01:17:23 +0000

    signed in user can add a marked litter event which other users will be able to see

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,43 @@
 var express = require('express');
 var router = express.Router();
 const isSignedIn = require("./middleware").isSignedIn
+const EventEmitter = require("events")
+class MarkerEmitter extends EventEmitter {} 
+const markerEmitter = new MarkerEmitter() 
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+let markers = []; // [refactor] perhaps call these litter_event or litter_location or something 
+
 // maybe rename this route to something better e.g. add-litter-location 
-router.post('/add-marker', isSignedIn, (req, res) => {
-  const { lat, lng } = req.body 
-  console.log(`Lat: ${lat}, ${lng}`)
+router.post('/add-marker', isSignedIn, async (req, res) => {
+  const { userId, lat, lng } = req.body
+  const newMarker = {userId, lat: req.body.lat, lng: req.body.lng}  
+  markers.push(newMarker);
+  markerEmitter.emit("marker", newMarker) 
+  console.log(` User: ${newMarker.userId} Lat: ${newMarker.lat}, Lng: ${newMarker.lng}`)
+})  
+
+router.get("/events", (req, res) => {
+  res.writeHead(200, {
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache"
+    });
+  markerEmitter.on("marker", (marker) => { // [refactor] there may be a better way of getting the markers 
+    res.write(`data: ${JSON.stringify({marker})}`);
+    res.write("\n\n");
+    res.end();
+  })
 })
 
+// [todo] cleanup e.g. handle connection close (on sign off?)
+router.get("/disconnect-map", (req, res) => {
+
+})
 
 module.exports = router;

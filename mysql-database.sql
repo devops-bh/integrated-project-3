90408d54d220ba4623163548778bdaab01f8750a
devops-bh <2016bryh@gmail.com>
2023-03-22 20:23:25 +0000

    add sqllite-database.sql [ignore the other commit]

--- mysql-database.sql
+++ mysql-database.sql
@@ -1,66 +1,59 @@
--- import to SQLite by running: sqlite3.exe db.sqlite3 -init sqlite.sql
-
-PRAGMA journal_mode = MEMORY;
-PRAGMA synchronous = OFF;
-PRAGMA foreign_keys = OFF;
-PRAGMA ignore_check_constraints = OFF;
-PRAGMA auto_vacuum = NONE;
-PRAGMA secure_delete = OFF;
-BEGIN TRANSACTION;
-
 CREATE TABLE IF NOT EXISTS users(
-PRIMARY KEY(user_id),
-score INT NOT NULL DEFAULT 0,
-firstname TEXT NOT NULL,
-lastname TEXT NOT NULL,
-email TEXT NOT NULL,
-password TEXT NOT NULL,
-postcode TEXT,
-image_path TEXT,
-is_staff INT NOT NULL DEFAULT 0
+    user_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(user_id),
+    score INT NOT NULL DEFAULT 0,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL, 
+    password VARCHAR(30) NOT NULL, 
+    postcode VARCHAR(10), 
+    image_path VARCHAR(100),
+    is_staff INT NOT NULL DEFAULT 0
 );
+
+
+
 CREATE TABLE IF NOT EXISTS litter_locations(
-litter_location_id INT NOT NULL ,
-PRIMARY KEY(litter_location_id),
-CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
-recommended_volunteer_count INT,
-`coordinate` POINT NOT NULL,
-is_clean INT NOT NULL DEFAULT 0,
-image_path TEXT
-);
+    litter_location_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(litter_location_id),
+    user_id INT,
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
+    timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
+    recommended_volunteer_count INT, 
+    `coordinate` POINT NOT NULL,
+    is_clean INT NOT NULL DEFAULT 0,
+    image_path VARCHAR(100)
+); -- ENGINE=InnoDB; -- depends on the configuration of PHPMyAdmin 
+
 CREATE TABLE IF NOT EXISTS volunteers(
-volunteer_id INT NOT NULL ,
-PRIMARY KEY(volunteer_id),
-litter_location_id INT,
-CONSTRAINT FK_volunteer_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-CONSTRAINT FK_litter_location_id FOREIGN KEY (litter_location_id) REFERENCES litter_locations(litter_location_id) ON DELETE CASCADE
+    volunteer_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(volunteer_id),
+    user_id INT, 
+    litter_location_id INT, 
+    CONSTRAINT FK_volunteer_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
+    CONSTRAINT FK_litter_location_id FOREIGN KEY (litter_location_id) REFERENCES litter_locations(litter_location_id) ON DELETE CASCADE
 );
+
 CREATE TABLE IF NOT EXISTS posts(
-post_id INT NOT NULL ,
-PRIMARY KEY(post_id),
-CONSTRAINT FK_user_poster_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-title TEXT NOT NULL,
-timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
-image_path TEXT NOT NULL,
-content TEXT NOT NULL
-);
+    post_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(post_id), 
+    user_id INT,
+    CONSTRAINT FK_user_poster_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
+    title VARCHAR(50) NOT NULL,
+    timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
+    image_path VARCHAR(100) NOT NULL,
+    content VARCHAR(500) NOT NULL
+); 
+
 CREATE TABLE IF NOT EXISTS comments(
-comment_id INT NOT NULL ,
-post_id INT,
-CONSTRAINT FK_user_commenter_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
-PRIMARY KEY (comment_id),
-timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
-content TEXT NOT NULL
+    comment_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    post_id INT,
+    CONSTRAINT FK_user_commenter_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
+    CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
+    PRIMARY KEY (comment_id), 
+    timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
+    content VARCHAR(100) NOT NULL
 );
 
-
-
-
-
-COMMIT;
-PRAGMA ignore_check_constraints = ON;
-PRAGMA foreign_keys = ON;
-PRAGMA journal_mode = WAL;
-PRAGMA synchronous = NORMAL;
+-- INSERT INTO `users`(`user_id`, `score`, `firstname`, `lastname`, `email`, `password`, `postcode`, `image_path`, `is_staff`) VALUES (DEFAULT, 1,'ross','lecturer','ross@ip3.com','password','postcode','',1)
--- sqllite-database.sql
+++ sqllite-database.sql
@@ -1,59 +1,66 @@
+-- import to SQLite by running: sqlite3.exe db.sqlite3 -init sqlite.sql
+
+PRAGMA journal_mode = MEMORY;
+PRAGMA synchronous = OFF;
+PRAGMA foreign_keys = OFF;
+PRAGMA ignore_check_constraints = OFF;
+PRAGMA auto_vacuum = NONE;
+PRAGMA secure_delete = OFF;
+BEGIN TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS users(
-    user_id INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(user_id),
-    score INTEGER NOT NULL DEFAULT 0,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL, 
-    password VARCHAR(30) NOT NULL, 
-    postcode VARCHAR(10), 
-    image_path VARCHAR(100),
-    is_staff INTEGER NOT NULL DEFAULT 0
+PRIMARY KEY(user_id),
+score INT NOT NULL DEFAULT 0,
+firstname TEXT NOT NULL,
+lastname TEXT NOT NULL,
+email TEXT NOT NULL,
+password TEXT NOT NULL,
+postcode TEXT,
+image_path TEXT,
+is_staff INT NOT NULL DEFAULT 0
 );
-
-
-
 CREATE TABLE IF NOT EXISTS litter_locations(
-    litter_location_id INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(litter_location_id),
-    user_id INTEGER,
-    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
-    timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
-    recommended_volunteer_count INTEGER, 
-    `coordinate` POINT NOT NULL,
-    is_clean INTEGER NOT NULL DEFAULT 0,
-    image_path VARCHAR(100)
-); -- ENGINE=InnoDB; -- depends on the configuration of PHPMyAdmin 
-
+litter_location_id INT NOT NULL ,
+PRIMARY KEY(litter_location_id),
+CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
+recommended_volunteer_count INT,
+`coordinate` POINT NOT NULL,
+is_clean INT NOT NULL DEFAULT 0,
+image_path TEXT
+);
 CREATE TABLE IF NOT EXISTS volunteers(
-    volunteer_id INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(volunteer_id),
-    user_id INTEGER, 
-    litter_location_id INTEGER, 
-    CONSTRAINT FK_volunteer_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
-    CONSTRAINT FK_litter_location_id FOREIGN KEY (litter_location_id) REFERENCES litter_locations(litter_location_id) ON DELETE CASCADE
+volunteer_id INT NOT NULL ,
+PRIMARY KEY(volunteer_id),
+litter_location_id INT,
+CONSTRAINT FK_volunteer_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+CONSTRAINT FK_litter_location_id FOREIGN KEY (litter_location_id) REFERENCES litter_locations(litter_location_id) ON DELETE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS posts(
-    post_id INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(post_id), 
-    user_id INTEGER,
-    CONSTRAINT FK_user_poster_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
-    title VARCHAR(50) NOT NULL,
-    timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
-    image_path VARCHAR(100) NOT NULL,
-    content VARCHAR(500) NOT NULL
-); 
-
+post_id INT NOT NULL ,
+PRIMARY KEY(post_id),
+CONSTRAINT FK_user_poster_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+title TEXT NOT NULL,
+timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
+image_path TEXT NOT NULL,
+content TEXT NOT NULL
+);
 CREATE TABLE IF NOT EXISTS comments(
-    comment_id INTEGER NOT NULL AUTO_INCREMENT,
-    user_id INTEGER,
-    post_id INTEGER,
-    CONSTRAINT FK_user_commenter_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE, 
-    CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
-    PRIMARY KEY (comment_id), 
-    timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
-    content VARCHAR(100) NOT NULL
+comment_id INT NOT NULL ,
+post_id INT,
+CONSTRAINT FK_user_commenter_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
+PRIMARY KEY (comment_id),
+timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
+content TEXT NOT NULL
 );
 
--- INSERT INTO `users`(`user_id`, `score`, `firstname`, `lastname`, `email`, `password`, `postcode`, `image_path`, `is_staff`) VALUES (DEFAULT, 1,'ross','lecturer','ross@ip3.com','password','postcode','',1)
+
+
+
+
+COMMIT;
+PRAGMA ignore_check_constraints = ON;
+PRAGMA foreign_keys = ON;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;

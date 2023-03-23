0a1d5636d4fc8781e5e1b4d88e16183c7de83834
devops-bh <2016bryh@gmail.com>
2023-03-23 22:03:38 +0000

    stripped out use of mysql2

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,6 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require("bcrypt")
 const isSignedIn = require("./middleware").isSignedIn
-const mysql = require('mysql2');
-
-// create the connection to database
-// also see www.npmjs.com/package/mysql2#user-content-using-connection-pool
-require('dotenv').config()
-const indexConnection = mysql.createConnection({ // stupidly creating a new connection for quickness 
- host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT
-});
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -34,15 +22,7 @@ router.post('/register', async (req, res) => {
   bcrypt.hash(password, 10).then(_hash =>{
     hashedPassword = _hash
     console.log(hashedPassword)
-    indexConnection.execute('INSERT INTO `users` (`user_id`, `score`, `firstname`, `lastname`, `email`, `password`, `postcode`, `image_path`, `is_staff`) VALUES (DEFAULT, DEFAULT, ?, ?, ?, ?, ?, ?, ?)',
-         //[email + "firstname", "Bobby", email, hashedPassword, "postcode2", "", 0],
-         [firstname, lastname, email, hashedPassword, "Postcode1", "", 0],
-        (err, results, fields) => { 
-            if (err) console.log(err); 
-            console.log("Registeration: ", results, fields)
-            // respond to the user telling them they've been registered to our amazing web app 
-            res.send("Check the database") // [todo] res.render the sign in form 
-          });
+
   })
 })
 
@@ -71,49 +51,7 @@ router.get('/userprofile', (req, res) => {
 router.get('/sign-in', (req, res) => {
   res.render('sign-in', { title: 'Sign in', signed_in: false })
 }) 
-/* 
-indexConnection.query('SELECT * FROM `users` where email="testone@testone.testone"', 
-(err, results, fields) => { 
-  if (err) console.log(err); 
-  console.log(results)
-  console.log(fields)
-  results.forEach(row => console.log(row.firstname))
-});
-indexConnection.execute(
-  'SELECT * FROM `users` WHERE `email` = ?',
-  ["testone@testone.testone"], (error, results, fields) => {
-      if (error) console.log("Error: ", error); 
-      console.log(results) // []
-      console.log(fields) 
-      // we care about grabbing the user using the email that was put into the form 
-      // at the moment we are trying to get a list of users from the database 
-      // I dont know if we are getting a single item list or a single item
-      //results.forEach(row => console.log(row.firstname))
-      /* 
-      bcrypt.compare(req.body.password, hashedPassword).then(function(result) {
-        console.log(hashedPassword, req.body.password)
-          if (result) req.session.signed_in = true 
-          console.log("user has just signed in: ", result) 
-          res.render('index', {title: `signed in as ${req.session.signed_in ? 'ross ': 'anon'}`, signed_in: req.session.signed_in, /* [todo] user.user_id *}) 
-      }).catch(compareErr => console.log(compareErr));
-      *
-    });
-  */ 
 
-// example of getting the user from the database using the email which was inputted into the sign in form 
-//indexConnection.execute('SELECT * FROM `users` WHERE email=?', ["testingantisqlinjection@email."],
-/* 
-indexConnection.execute('SELECT * FROM `users` WHERE email=?', ["testingantisqlinjection@email."], 
-  (err, results, fields) => { 
-    if (err) console.log(err)
-    // console.log(results) // an array of one user e.g. [{user}]
-    //results.forEach(result => console.log(result)) // 
-    // alternatively you could do for (let i ...) { let result = results[i] }
-    // since we are assuming emails are unique, you can simply do 
-    const user = results[0]
-    console.log("USER: ", user)
-});
-*/
 let hashedPassword; 
 bcrypt.hash("lecturer", 10).then(_hash =>{
   hashedPassword = _hash
@@ -128,35 +66,6 @@ router.post('/sign-in', isSignedIn, (req, res) => {
     res.redirect('/') // Redirect to reportLitter[todo] implement warning using the connect-flash package (I think res.render should be used to get rid of the resubmit form message)
     return;
   }
-  /* 
-  [{
-    username: kjiosjsd
-  }, {username: owkdasopksdop}]
-
-  <xml>
-    <result>
-      <object><username>kjiosjsd</username></object>
-      <object><username>kjiosjsd</username></object>
-      <object><username>kjiosjsd</username></object>
-      <object><username>kjiosjsd</username></object>
-    </result>
-  </xml>
-  */
-    connection.execute(
-  'SELECT * FROM `users` WHERE `email` = ?',
-  [email], (error, results, fields) => {
-      if (err) console.log("Error: ", err); 
-      console.log(fields) 
-      //results.forEach(row => console.log(row.firstname))
-      /* 
-      bcrypt.compare(req.body.password, hashedPassword).then(function(result) {
-        console.log(hashedPassword, req.body.password)
-          if (result) req.session.signed_in = true 
-          console.log("user has just signed in: ", result) 
-          res.render('index', {title: `signed in as ${req.session.signed_in ? 'ross ': 'anon'}`, signed_in: req.session.signed_in, /* [todo] user.user_id *}) 
-      }).catch(compareErr => console.log(compareErr));
-      */
-    });
 })
 
 router.get('/sign-off', isSignedIn, (req, res) => {

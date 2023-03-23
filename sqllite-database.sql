b50fc8adddcfa9b77cadc89faa63978bd166badf
devops-bh <2016bryh@gmail.com>
2023-03-23 20:36:13 +0000

    updated sqllite queries

--- sqllite-database.sql
+++ sqllite-database.sql
@@ -9,6 +9,7 @@ PRAGMA secure_delete = OFF;
 BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS users(
+user_id INT NOT NULL AUTOINCREMENT,
 PRIMARY KEY(user_id),
 score INT NOT NULL DEFAULT 0,
 firstname TEXT NOT NULL,
@@ -20,7 +21,7 @@ image_path TEXT,
 is_staff INT NOT NULL DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS litter_locations(
-litter_location_id INT NOT NULL ,
+litter_location_id INT NOT NULL AUTOINCREMENT,
 PRIMARY KEY(litter_location_id),
 CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
 timestamp_posted TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -30,14 +31,14 @@ is_clean INT NOT NULL DEFAULT 0,
 image_path TEXT
 );
 CREATE TABLE IF NOT EXISTS volunteers(
-volunteer_id INT NOT NULL ,
+volunteer_id INT NOT NULL AUTOINCREMENT,
 PRIMARY KEY(volunteer_id),
 litter_location_id INT,
 CONSTRAINT FK_volunteer_user_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
 CONSTRAINT FK_litter_location_id FOREIGN KEY (litter_location_id) REFERENCES litter_locations(litter_location_id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS posts(
-post_id INT NOT NULL ,
+post_id INT NOT NULL AUTOINCREMENT,
 PRIMARY KEY(post_id),
 CONSTRAINT FK_user_poster_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
 title TEXT NOT NULL,
@@ -46,7 +47,7 @@ image_path TEXT NOT NULL,
 content TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS comments(
-comment_id INT NOT NULL ,
+comment_id INT NOT NULL AUTOINCREMENT,
 post_id INT,
 CONSTRAINT FK_user_commenter_id FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
 CONSTRAINT FK_post_id FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,

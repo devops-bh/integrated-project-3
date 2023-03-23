79f491efcd9ceaf659ffeb2ff784add115e0b940
devops-bh <2016bryh@gmail.com>
2023-03-23 23:17:59 +0000

    implemented sign in functionality using the JSON file as the database and JS instead of SQL

--- routes/index.js
+++ routes/index.js
@@ -77,7 +77,18 @@ router.post('/sign-in', isSignedIn, (req, res) => {
     res.redirect('/') // Redirect to reportLitter[todo] implement warning using the connect-flash package (I think res.render should be used to get rid of the resubmit form message)
     return;
   }
-  res.send("todo")
+      fs.readFile(path.join(__dirname, "./database.json"), 'utf8', (err, data) => {
+        if (err) console.log(err)
+        const parsedData = JSON.parse(data)
+        // assuming the user is signed in; [todo] should probably handle cases where the inputted email does not exist
+        parsedData.users.map(user => {
+          bcrypt.compare(req.body.password, user.password).then(function(result) {
+            if (result) req.session.signed_in = true 
+              console.log("user has just signed in: ", result) 
+              res.render('index', {title: `signed in as ${req.session.signed_in ? 'ross ': 'anon'}`, signed_in: req.session.signed_in, /* [todo] user.user_id */}) 
+          }).catch(compareErr => console.log(compareErr));
+        })
+    })
 })
 
 
